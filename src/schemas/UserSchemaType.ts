403229c0fad85d6418c458c55742090f08fb7316
Aharon Dzhuraev <dzhuraev@gmail.com>
2024-05-28 10:18:14 +0300

    Update UserSchemaType.ts

--- src/schemas/UserSchemaType.ts
+++ src/schemas/UserSchemaType.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const UserSchema = z.object({
     id: z.number().gte(1, 'id must greater than 0'),
     amount: z.number().gte(1, 'amount must greater than 0'),
-    periodInMonths: z.number().gte(1, 'month must greater than 1'),
+    periodInMonths: z.number().gte(12, 'month must greater or equal than 12')
 });
 
 export type UserSchemaType = z.output<typeof UserSchema>;
